f089b7969c176e3818a82709a41f2e689778f0c2
Jonathan Ansley <ansley.jonathan.e@gmail.com>
2017-06-19 14:59:31 -0400

    not working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,146 +1,214 @@
 // This code will run as soon as the page loads
 window.onload = function() {
-  // alert('Would you like to play a game? - window.onload');
-  restart();
+  // alert('JavaScript file - window.onload');
+  // restart();
+
   };
 
 
 // Variables
 // =======================================================
 var game = {
-  winsNumber: 0,
-  lossesNumber: 0,
-  crystalOneValue: getRandomIntInclusive(1, 12),
-  crystalTwoValue: getRandomIntInclusive(1, 12),
-  crystalThreeValue: getRandomIntInclusive(1, 12),
-  crystalFourValue: getRandomIntInclusive(1, 12),
-  goalNumber: getRandomIntInclusive(19, 120),
-  userNumber: 0,
+  correctAnswerCount: 0,
+  incorrectAnswerCount: 0,
+  timeCounter: 20,
+  // timeCounter: 120;
+  // unansweredCount: 0,
+  // userAnswer: '',
+  countDown: function(){
+    game.timeCounter--;
+    $('#counter').html(game.timeCounter);
+    if (game.timeCounter === 0)
+    {
+      console.log('out of time');
+      game.done();
+    }
+  }
 };
 
-// Functions
-// =======================================================
+var questions = [
+  {
+  question: 'Who said, Of all the souls I have encountered in my travels, his was the most....human.',
+  answers:[ 'Lt. Worf', 'Mr. Sulu', 'Mr. Spock' ],
+  correctAnswer: 'James T. Kirk'
+  },
 
-function displayGoalNumber() {
-  // $(#goal).html.("Goal Number: " + game.goalNumber);
-  $('#goal').html("Goal Number: " + game.goalNumber);
-  // console.log('displayGoalNumber function' + game.goalNumber);
-}
+  {
+  question: 'Who said, \"We\'re not here to take over. We just want to sell you things.\"',
+  answers:[ 'Lt. Worf', 'Mr. Sulu', 'Mr. Spock' ],
+  correctAnswer: 'Quark the Ferengi'
+  },
 
-function displayUserNumber() {
-  $('#user-number').html("User Number: " + game.userNumber);
-  console.log('displayUserNumber function');
-}
+  {
+  question: 'Having is not so pleasing a thing as wanting, it may not be logical but it is often true.',
+  answers:[ 'Mr. Sulu', 'Lt. Uhura', 'Number One' ],
+  correctAnswer: 'Mr. Spock'
+  },
 
-function displayWins() {
-  $('#wins').html("Wins: " + game.winsNumber);
-  console.log('displayWins function');
-}
+  {
+  question: 'Jim (Captain Kirk), you don\’t go around asking the Almighty for his I.D.!',
+  answers:[ 'Lt. Worf', 'Mr. Sulu', 'Mr. Spock' ],
+  correctAnswer: 'Dr ‘Bones’ McCoy'
+  },
 
-function displayLosses() {
-  $('#losses').html("Losses: " + game.lossesNumber);
-  console.log('displayLosses function');
-}
+  {
+  question: 'I\'m a doctor, not a bricklayer',
+  answers:[ 'Lt. Worf', 'Mr. Sulu', 'Mr. Spock' ],
+  correctAnswer: 'Dr ‘Bones’ McCoy'
+  },
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-  console.log('getRandomIntInclusive function');
-}
+  // {
+  // question: '',
+  // answers:[ 'Lt. Worf', 'Mr. Sulu', 'Mr. Spock' ],
+  // correctAnswer: ''
+  // },
+
+  // {
+  // question: '',
+  // answers:[ 'Lt. Worf', 'Mr. Sulu', 'Mr. Spock' ],
+  // correctAnswer: ''
+  // },
+
+  // {
+  // question: '',
+  // answers:[ 'Lt. Worf', 'Mr. Sulu', 'Mr. Spock' ],
+  // correctAnswer: ''
+  // }
+
+  ];
+
+
+// // Functions
+// // =======================================================
+
+function displayCorrectAnswerCount() {
+    alert('This is from the displayCorrectAnswerCount function.');
+//   // $(#goal).html.("Goal Number: " + game.goalNumber);
+//   $('#goal').html("Goal Number: " + game.goalNumber);
+//   // console.log('displayGoalNumber function' + game.goalNumber);
+};
+
+function displayIncorrectAnswerCount() {
+  alert('This is from the displayCorrectAnswerCount function.');
+//   $('#user-number').html("User Number: " + game.userNumber);
+//   console.log('displayUserNumber function');
+};
+
+function displayIncorrectAnswerCount() {
+  alert('This is from the displayUnansweredCount function.');
+//   $('#wins').html("Wins: " + game.winsNumber);
+//   console.log('displayWins function');
+};
+
  
-function evaluate() {
-  console.log('evaluate function');
+// function evaluate() {
+//   console.log('evaluate function');
 
-  if (game.goalNumber === game.userNumber) {
-    game.winsNumber++;
+//   if (game.goalNumber === game.userNumber) {
+//     game.winsNumber++;
 
-  //display updated score
-  displayWins();
-  alert('You won.');
+//   //display updated score
+//   displayWins();
+//   alert('You won.');
 
-  restart();
+//   start();
+//   }
+
+//   else if (game.userNumber >= game.goalNumber) {
+//   game.lossesNumber++;
+  
+//   //display updated score
+//   displayLosses();
+//   alert('You lost.');
+
+//   start();
+//   }
+
+// };
+ 
+// function start() {
+//   console.log('This is from the start function.');
+//   game.userNumber = 0,
+
+
+
+  // game.correctAnswerCount = 0;
+  // game.incorrectAnswerCount = 0;
+  // game.unansweredCount = 0;
+  // userAnswer: '',
+  
+//   //reset value of crystal ids
+//   game.crystalOneValue = getRandomIntInclusive(1, 12),
+//   $('#crystal-1').val(game.crystalOneValue);
+//   console.log('#crystal-1 = ' + game.crystalOneValue);
+
+//   displayGoalNumber();
+
+//   displayUserNumber();
+
+//   displayWins();
+
+//   displayLosses();
+
+// };
+
+
+
+// <button type="button" class="btn btn-success">answer 1</button>
+
+// <button type="button" class="btn btn-primary" id = "done-button">Done</button>
+
+ 
+// // Main Process
+// // =======================================================
+
+$('#start-button').on('click', function() {
+  console.log('You clicked #start-button');
+
+  //after user clicks start button
+  $('#start-button').remove();
+
+  for (var i = questions.length - 1; i >= 0; i--) {
+    $('#subwrapper').append('<h2>' + questions[i].question + '</h2>');
+
+      for (var j = questions.[i].answers.length - 1; i >= 0; i--) {
+        $('#subwrapper').append("<input type = 'radio' name = 'question-" +i + "' value='" +questions[i].answers[j]+"'>"+questions[i].answers[j])
+
   }
 
-  else if (game.userNumber >= game.goalNumber) {
-  game.lossesNumber++;
-  
-  //display updated score
-  displayLosses();
-  alert('You lost.');
 
-  restart();
-  }
-
-};
- 
-function restart() {
-  console.log('This is from the restart function.');
-  game.goalNumber = getRandomIntInclusive(19, 120),
-  game.userNumber = 0,
-  
-  //reset value of crystal ids
-  game.crystalOneValue = getRandomIntInclusive(1, 12),
-  $('#crystal-1').val(game.crystalOneValue);
-  console.log('#crystal-1 = ' + game.crystalOneValue);
-
-  game.crystalTwoValue = getRandomIntInclusive(1, 12),
-  $('#crystal-2').val(game.crystalTwoValue);
-  console.log('#crystal-2 = ' + game.crystalTwoValue);
-
-  game.crystalThreeValue = getRandomIntInclusive(1, 12);
-  $('#crystal-3').val(game.crystalThreeValue);
-  console.log('#crystal-3 = ' + game.crystalThreeValue);
-
-  $('#crystal-4').val(game.crystalFourValue);
-  console.log('#crystal-4 = ' + game.crystalFourValue);
-  game.crystalFourValue = getRandomIntInclusive(1, 12);
-
-  displayGoalNumber();
-
-  displayUserNumber();
-
-  displayWins();
-
-  displayLosses();
-
-};
- 
-// Main Process
-// =======================================================
-
-$('#crystal-1').click(function() {
-  console.log('You clicked crystal-1');
-  game.userNumber = game.userNumber + game.crystalOneValue;
-  displayUserNumber();
-  evaluate();
+//   game.userNumber = game.userNumber + game.crystalOneValue;
+//   displayUserNumber();
+//   evaluate();
   });
 
-$('#crystal-2').click(function() {
-  console.log('You clicked crystal-2');
-  game.userNumber = game.userNumber + game.crystalTwoValue;
-  displayUserNumber();
-  evaluate();
+
+
+$('#done-button').on('click', function() {
+  console.log('You clicked #done-button');
+//   game.userNumber = game.userNumber + game.crystalOneValue;
+//   displayUserNumber();
+//   evaluate();
   });
 
-$('#crystal-3').click(function() {
-  console.log('You clicked crystal-3');
-  game.userNumber = game.userNumber + game.crystalThreeValue;
-  displayUserNumber();
-  evaluate();
-  });
 
-$('#crystal-4').click(function() {
-  console.log('You clicked crystal-4');
-  game.userNumber = game.userNumber + game.crystalFourValue;
-  displayUserNumber();
-  evaluate();
-  });
+this.results();
+},
 
-displayGoalNumber();
+result: function(){
+  clearInterval(timer);
+  $('#subwrapper h2').remove();
 
-displayUserNumber();
+  $('#subwrapper').append('<h3>')
 
-displayWins();
+}
 
-displayLosses();
+
+
+// displayGoalNumber();
+
+// displayUserNumber();
+
+// displayWins();
+
+// displayLosses();
